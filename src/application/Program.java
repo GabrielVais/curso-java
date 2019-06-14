1acff3133b67d3c453b624d6d4ba512ddbaed6d1
gabrielVais <gabriel_lol2017@hotmail.com>
2019-06-14 20:28:17 -0300

    start to implement board and piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
+import boardgame.Board;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world");
+		Board board = new Board(8, 8);
 
 	}
 
