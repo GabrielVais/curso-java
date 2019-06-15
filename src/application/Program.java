140feea6bc42a6cbeaaf58085ee73beecfe877ed
gabrielVais <gabriel_lol2017@hotmail.com>
2019-06-15 00:50:27 -0300

    adcionado peças ao tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 import boardgame.Board;
+import chess.ChessMatch;
 
 
 public class Program {
@@ -7,7 +8,11 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
+		
+		
 
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,6 +33,30 @@ public class Board {
 	}
 	
 	
+	//metodo para retornar peça a coluna
+	
+	
+	public Piece piece(int row, int column) {
+		
+		return pieces[row][column];
+		
+	}
+	
+	public Piece piece(Position position) {
+		
+		return pieces[position.getRow()][position.getColumn()];
+		
+		
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		
+		pieces[position.getRow()][position.getColumn()] = piece;
+		
+		piece.position = position;
+		
+		
+	}
 	
 
 }
