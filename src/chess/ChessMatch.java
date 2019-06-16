e3020937a20040d2f8086679f4151c74573e9517
gabrielVais <gabriel_lol2017@hotmail.com>
2019-06-16 00:30:11 -0300

    adcionado a classe Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -284,12 +285,13 @@ public class ChessMatch {
 	
 	//colocando as peças no tabuleiro
 	private void initialSetup() {
-		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
+		placeNewPiece('h', 4, new Rook(board, Color.WHITE));
 		placeNewPiece('d', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
-		;
+		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+		
    
-   
+		placeNewPiece('h', 7, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new King(board, Color.BLACK));
 	}
